df3862df7a70d5780f3b1bfd1786de07dbd0b1af
ViniNovack <vininomn@gmail.com>
2026-09-07 15:17:33 -0300

    Atualização

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -23,7 +23,7 @@ const RevealText = ({
         y: 0,
         filter: "blur(0px)",
       }}
-      viewport={{ amount: 0.4, once: false, margin: "-15% 0px -15% 0px" }}
+      viewport={{ amount: 0.7, once: false }}
       transition={{ duration: 0.6, ease: "easeOut", delay }}
     >
       {children}
