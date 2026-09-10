ad05ffbf4098bbcd4c9a45f8f6cf986c923f516a
ViniNovack <vininomn@gmail.com>
2026-09-10 16:09:04 -0300

    Atualiza informações do Data ICMC

--- src/data/education.ts
+++ src/data/education.ts
@@ -110,9 +110,10 @@ export const schools: School[] = [
   {
     id: "cursos-especializacao-rapida",
     title: "Data ICMC",
+    website: "https://data.icmc.usp.br/",
     tags: ["CURSOS"],
-    year: "2023",
-    description: "",
+    year: "2026",
+    description: "O Data ICMC é um grupo de extensão do Instituto de Ciências Matemáticas e de Computação ICMC-USP da Universidade de São Paulo USP localizado no campus de São Carlos. Fundado em 2019, o grupo é focado em Ciência de Dados e Aprendizado de Máquina. O Data busca integrar estudantes, acadêmicos e profissionais do setor, promovendo a troca de conhecimento e o desenvolvimento de habilidades técnicas. Através de aulas, eventos, grupos de estudo e palestras, o grupo facilita o acesso a tecnologias de ponta e práticas atuais, preparando seus membros para os desafios do mercado de trabalho.",
     coverImage: tarotCards,
     images: [tarotCards],
     courses: [
