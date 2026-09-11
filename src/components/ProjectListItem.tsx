6fcfb73a63909b286160ca0d045b7646cb627fa2
ViniNovack <vininomn@gmail.com>
2026-09-11 16:32:57 -0300

    style: move project hover images closer to titles

--- src/components/ProjectListItem.tsx
+++ src/components/ProjectListItem.tsx
@@ -66,7 +66,7 @@ export function ProjectListItem({
 
           {/* Hover Image - positioned relative to the hovered project row */}
           <div
-            className={`absolute left-[52%] top-1/2 -translate-y-1/2 w-48 sm:w-56 md:w-64 lg:w-80 max-h-[70vh] flex items-center justify-center pointer-events-none z-40 transition-all duration-300 ${
+            className={`absolute left-[46%] top-1/2 -translate-y-1/2 w-48 sm:w-56 md:w-64 lg:w-80 max-h-[70vh] flex items-center justify-center pointer-events-none z-40 transition-all duration-300 ${
               isHovered
                 ? "opacity-100 translate-x-0"
                 : "opacity-0 translate-x-4"
