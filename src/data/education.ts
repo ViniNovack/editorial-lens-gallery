85a6ab1bf3afec5451c34081ff724c4028b345d2
ViniNovack <vininomn@gmail.com>
2026-09-25 17:07:13 -0300

    fix: load Python course screenshot from repository asset URL

--- src/data/education.ts
+++ src/data/education.ts
@@ -26,7 +26,7 @@ import destPatrocinio from "@/assets/dest-patrocinio.png";
 import imgWa0030 from "@/assets/IMG-20241016-WA0030.jpg";
 import imgWa0014 from "@/assets/IMG-20241016-WA0014.jpg";
 import maxresdefault from "@/assets/maxresdefault.jpg";
-import capturaPython from "@/assets/Captura de tela 2026-09-20 101332.png";
+const capturaPython = "https://raw.githubusercontent.com/ViniNovack/editorial-lens-gallery/main/src/assets/Captura%20de%20tela%202026-09-20%20101332.png";
 import capaIA from "@/assets/capa_IA.png";
 import modulosIA from "@/assets/modulos_IA.png";
 import capaSQL from "@/assets/capa_SQL.png";
